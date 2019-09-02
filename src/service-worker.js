a8d0b71d1bbaf21e2be6385b008d16fb67e1bf91
lilinsen <lilinsen@jd.com>
2019-09-02 11:27:23 +0800

    提交

--- src/service-worker.js
+++ src/service-worker.js
@@ -11,30 +11,32 @@ self.addEventListener('install', event => {
     })
   );
 });
-  
+let cacheList =[
+
+]
 /**
 为 fetch 事件添加一个事件监听器。接下来，使用 caches.match() 函数来检查传入的请求 URL 是否匹配当前缓存中存在的任何内容。如果存在的话，返回缓存的资源。
 如果资源并不存在于缓存当中，通过网络来获取资源，并将获取到的资源添加到缓存中。
 */
-// self.addEventListener('fetch', function (event) {
-//   event.respondWith(
-//     caches.match(event.request)                  
-//     .then(function (response) {
-//         if (response) {                            
-//             return response;                         
-//         }
-//         var requestToCache = event.request.clone();  //          
-//         return fetch(requestToCache).then(                   
-//             function (response) {
-//             if (!response || response.status !== 200) {      
-//                 return response;
-//             }
-//             var responseToCache = response.clone();          
-//             caches.open(cacheName)                           
-//                 .then(function (cache) {
-//                 cache.put(requestToCache, responseToCache);  
-//                 });
-//             return response;             
-//         })
-//     }))
-// });
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)                  
+    .then(function (response) {
+        if (response) {                            
+            return response;                         
+        }
+        var requestToCache = event.request.clone();  //          
+        return fetch(requestToCache).then(                   
+            function (response) {
+            if (!response || response.status !== 200) {      
+                return response;
+            }
+            var responseToCache = response.clone();          
+            caches.open(cacheName)                           
+                .then(function (cache) {
+                cache.put(requestToCache, responseToCache);  
+                });
+            return response;             
+        })
+    }))
+});
